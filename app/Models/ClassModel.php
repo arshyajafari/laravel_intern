f3283271de82b2e4f73ef088d60bdfe427c9a73c
Arshya Jafari <arshyajafari11@gmail.com>
2022-11-03 23:30:59 +0330

    Added class model and student model

---
+++ app/Models/ClassModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ClassModel extends Model
+{
+    use HasFactory;
+
+    protected $table = "classes";
+
+    protected $fillable = [
+        "name",
+        "description"
+    ];
+
+    public function studentInClasses ()
+    {
+        return $this->belongsToMany(StudentModel::class, 'student_class', 'class_id', 'student_id');
+    }
+}
---
+++ app/Models/StudentModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class StudentModel extends Model
+{
+    use HasFactory;
+
+    protected $table = "students";
+
+    protected $fillable = [
+        "full_name",
+        "email",
+        "phone_number",
+        "national_code",
+        "birth_date",
+        "state",
+        "city",
+        "gender",
+        "password",
+        "class_id",
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    public function studentInClass()
+    {
+        return $this->belongsTo(ClassModel::class, "class_id", "id");
+    }
+
+    public function studentsInClass ()
+    {
+        return $this->belongsToMany(ClassModel::class, 'student_class', 'student_id', 'class_id');
+    }
+}
