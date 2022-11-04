b3fc49c73ed2bc1ff81d19b287b5f765969c04cb
Arshya Jafari <arshyajafari11@gmail.com>
2022-11-04 14:47:37 +0330

    Changing class id type from string to json

---
+++ database/migrations/2022_11_04_110435_create_students_table.php
@@ -0,0 +1,41 @@
+<?php
+
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
+
+    return new class extends Migration
+    {
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up()
+        {
+            Schema::create('students', function (Blueprint $table) {
+                $table->id();
+                $table->string("full_name", 150)->nullable(false);
+                $table->string("email", 100)->nullable(false);
+                $table->string("phone_number", 20)->nullable(false);
+                $table->string("national_code", 15)->nullable(false);
+                $table->string("birth_date", 15)->nullable(false);
+                $table->string("state", 100)->nullable(false);
+                $table->string("city", 100)->nullable(false);
+                $table->string("gender", 100)->nullable(false);
+                $table->string("password", 100)->nullable(false);
+                $table->json("class_id");
+                $table->timestamps();
+            });
+        }
+
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('students');
+        }
+    };
